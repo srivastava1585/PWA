f51f7ba50c0f05968e92eccd89d0a149f34d6586
srivastava1585 <srivastava.p@hcl.com>
2019-09-20 19:34:38 +0530

    file updated

--- PWADemo/sw.js
+++ PWADemo/sw.js
@@ -1,76 +1,37 @@
-<<<<<<< HEAD
-var cacheName = 'PSoC'; 
-  
+var cacheName = 'PWA';
+
 var filesToCache = [
- 
-'/',    
- 
-'/Account/Login', 
-   
+
+'/',
+
+'/Account/Login',
+
 '/Scripts/main.js'];
-    
-self.addEventListener('install', function(e) { 
- 
-e.waitUntil(
-       
-caches.open(cacheName).then(function(cache) { 
-        
-return cache.addAll(filesToCache);   
-    
-})    
- 
-);  
- 
-}); 
-    
-/* Serve cached content when offline */ 
-  
-self.addEventListener('fetch', function(e) {  
-   
-e.respondWith(      caches.match(e.request).then(function(response) {  
-       
-return response || fetch(e.request);
-       
-})   
-  
-);  
- 
-=======
-var cacheName = 'PSoC'; 
-  
-var filesToCache = [
- 
-'/',    
- 
-'/Account/Login', 
-   
-'/Scripts/main.js'];
-    
-self.addEventListener('install', function(e) { 
- 
-e.waitUntil(
-       
-caches.open(cacheName).then(function(cache) { 
-        
-return cache.addAll(filesToCache);   
-    
-})    
- 
-);  
- 
-}); 
-    
-/* Serve cached content when offline */ 
-  
-self.addEventListener('fetch', function(e) {  
-   
-e.respondWith(      caches.match(e.request).then(function(response) {  
-       
-return response || fetch(e.request);
-       
-})   
-  
-);  
- 
->>>>>>> 7ad166977e2cfc9a3bbb4eb9e38f22befb25c4f2
+
+self.addEventListener('install', function (e) {
+
+    e.waitUntil(
+
+    caches.open(cacheName).then(function (cache) {
+
+        return cache.addAll(filesToCache);
+
+    })
+
+    );
+
+});
+
+/* Serve cached content when offline */
+
+self.addEventListener('fetch', function (e) {
+
+    e.respondWith(caches.match(e.request).then(function (response) {
+
+        return response || fetch(e.request);
+
+    })
+
+    );
+
 });
