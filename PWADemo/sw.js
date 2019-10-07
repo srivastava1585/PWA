19bba1290d6396dd07a27fe60ccd52b2c8376890
POC <55582893+srivastava1585@users.noreply.github.com>
2019-10-07 05:35:44 -0400

    Offline support for images

--- PWADemo/sw.js
+++ PWADemo/sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'PWA';
+var cacheName = 'pwa-content-store-v1';
 
 var filesToCache = [
 '/',
@@ -12,31 +12,27 @@ var filesToCache = [
 '/Scripts/modernizr-2.6.2.js',
 '/Scripts/jquery-1.10.2.min.js',
 '/Scripts/bootstrap.js',
-'/Scripts/respond.js'];
+'/Scripts/respond.js',
+'/Assets/Images/Image1.jpg',
+'/Assets/Images/Image2.jpg',
+'/Assets/Images/Image3.jpg',
+'/Assets/Images/Image4.jpg'];
 
 self.addEventListener('install', function (e) {
-
     e.waitUntil(
-
     caches.open(cacheName).then(function (cache) {
-
         return cache.addAll(filesToCache);
-
     })
-
     );
-
 });
 
 /* Serve cached content when offline */
-
 self.addEventListener('fetch', function (e) {
-
     e.respondWith(caches.match(e.request).then(function (response) {
-
         return response || fetch(e.request);
-
     })
+    );
+});
 
     );
 
