3a2307807ed9ff62f6b53decb0948bf1b00f683c
srivastava1585 <srivastava.p@hcl.com>
2019-10-09 17:40:06 +0530

    offline with dynamic URLs

--- PWADemo/sw.js
+++ PWADemo/sw.js
@@ -1,26 +1,7 @@
 var cacheName = 'pwa-content-store-v1';
 
 var filesToCache = [
-<<<<<<< HEAD
 '/'];
-=======
-'/',
-'/Account/Login',
-'/Home/About',
-'/Home/Contact',
-'/Account/Register',
-'/Scripts/main.js',
-'/Content/bootstrap.css',
-'/Content/site.css',
-'/Scripts/modernizr-2.6.2.js',
-'/Scripts/jquery-1.10.2.min.js',
-'/Scripts/bootstrap.js',
-'/Scripts/respond.js',
-'/Assets/Images/Image1.jpg',
-'/Assets/Images/Image2.jpg',
-'/Assets/Images/Image3.jpg',
-'/Assets/Images/Image4.jpg'];
->>>>>>> ec19b402425391d331cdb38f39591294eaacb8df
 
 /* install app */
 self.addEventListener('install', function (e) {
@@ -33,7 +14,6 @@ self.addEventListener('install', function (e) {
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function (e) {
-<<<<<<< HEAD
     // Parse the URL:
     //const requestURL = new URL(e.request.url);
     e.respondWith(caches.match(e.request).then(function (response) {
@@ -54,19 +34,12 @@ self.addEventListener('activate', event => {
       caches.keys().then(keys => Promise.all(
         keys.map(key => {
             if (key !== cacheName) {
-                console.log(key + 'now ready to handle fetches!');
+                console.log(key + ' now ready to handle fetches!');
                 return caches.delete(key);
             }
         })
       )).then(() => {
-          console.log(cacheName + 'now ready to handle fetches!');
+          console.log(cacheName + ' now ready to handle fetches!');
       })
     );
 });
-=======
-    e.respondWith(caches.match(e.request).then(function (response) {
-        return response || fetch(e.request);
-    })
-    );
-});
->>>>>>> ec19b402425391d331cdb38f39591294eaacb8df
