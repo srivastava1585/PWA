49caa7f2afc212c6b0bf1d91533eb7cf2e74565d
srivastava1585 <srivastava.p@hcl.com>
2019-10-09 17:15:41 +0530

    update on service worker file

--- PWADemo/Scripts/main.js
+++ PWADemo/Scripts/main.js
@@ -1,15 +1,13 @@
-window.onload = () => {
-
-    'use strict';
-
-
-
-    if ('serviceWorker' in navigator) {
-
-        navigator.serviceWorker
-
-          .register('/sw.js');
-
-    }
-
+if ('serviceWorker' in navigator) {
+    // Register a service worker hosted at the root of the
+    // site using the default scope.
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js', { scope: './' }).then(function (registration) {
+            console.log('Service worker registration succeeded:', registration);
+        }, /*catch*/ function (error) {
+            console.log('Service worker registration failed:', error);
+        });
+    });
+} else {
+    console.log('Service workers are not supported.');
 }
--- PWADemo/sw.js
+++ PWADemo/sw.js
@@ -1,43 +1,45 @@
-var cacheName = 'PWA';
+var cacheName = 'pwa-content-store-v1';
 
 var filesToCache = [
-'/',
-'/Account/Login',
-'/Home/About',
-'/Home/Contact',
-'/Account/Register',
-'/Scripts/main.js',
-'/Content/bootstrap.css',
-'/Content/site.css',
-'/Scripts/modernizr-2.6.2.js',
-'/Scripts/jquery-1.10.2.min.js',
-'/Scripts/bootstrap.js',
-'/Scripts/respond.js'];
+'/'];
 
+/* install app */
 self.addEventListener('install', function (e) {
-
     e.waitUntil(
-
     caches.open(cacheName).then(function (cache) {
-
         return cache.addAll(filesToCache);
-
     })
-
     );
-
 });
 
 /* Serve cached content when offline */
-
 self.addEventListener('fetch', function (e) {
-
+    // Parse the URL:
+    //const requestURL = new URL(e.request.url);
     e.respondWith(caches.match(e.request).then(function (response) {
-
-        return response || fetch(e.request);
-
+        return response || fetch(e.request).then(function (r) {
+            response = r;
+            caches.open(cacheName).then(function (cache) {
+                cache.put(e.request, response);
+            });
+            return response.clone();
+        });
     })
-
     );
+});
 
+/* remove these cached files from user’s browser and replace with new ones */
+self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(keys => Promise.all(
+        keys.map(key => {
+            if (key !== cacheName) {
+                console.log(key + 'now ready to handle fetches!');
+                return caches.delete(key);
+            }
+        })
+      )).then(() => {
+          console.log(cacheName + 'now ready to handle fetches!');
+      })
+    );
 });
